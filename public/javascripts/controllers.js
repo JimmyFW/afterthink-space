d3f0855d6687122cd3f27cc133032619c88f65a9
JamesFW <james.wu@students.olin.edu>
2013-11-06 17:10:36 -0500

    menu is working

--- public/javascripts/controllers.js
+++ public/javascripts/controllers.js
@@ -3,27 +3,28 @@ var controllers = angular.module('afterthink.controllers', []);
 controllers.controller('MyCtrl', ['$scope', 'angularFire',
   function($scope, angularFire) {
     console.log("LOAD MyCtrl");
-    var url = 'https://groupthought.firebaseio.com/gamestate';
-    // $scope.items = angularFire(new Firebase(url), $scope, "items");
-    angularFire(new Firebase(url), $scope, "items")
-    //angularFire(new Firebase(url), $scope, "items");
 
-/*
+    var url = 'https://groupthought.firebaseio.com/gamestate';
+    angularFire(new Firebase(url), $scope, "items");
+
     var url = 'https://groupthought.firebaseio.com/sharedspace/dishes';
-    //$scope.dishes = 
     angularFire(new Firebase(url), $scope, "dishes");
 
     var url = 'https://groupthought.firebaseio.com/menu';
-    //$scope.menu = 
     angularFire(new Firebase(url), $scope, "menu");
-*/
 
     $scope.addItem = function (e) {
-      console.log("down!!");
-      if(e.keyCode == 13) {
-        $scope.items.push($scope.nextItem);
-        //$scope.nextItem = "";
-      }
+      if(e.keyCode != 13) return;
+      $scope.items.push($scope.nextItem);
+      $scope.nextItem = "";
+    }
+
+    $scope.deleteItem = function (e, index) {
+      if(index >= $scope.items.length) return;
+      // splice returns the elements removed and modifies the array in place
+      $scope.items.splice(index, 1); // remove one item after the index
+      $scope.debugIndex = index;
+      e.preventDefault();
     }
   }
 ]);
